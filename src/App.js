9cf5bfaecfa29c80f7cae9675216c9a20afed4b1
AbdulGaffarAhmed <112863356+AbdulGaffarAhmed@users.noreply.github.com>
2023-05-14 20:14:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [query,setQuery] = useState('');
+  fetch('https://api.github.com/users')
+  .then((res)=>res.json())
+  .then((json)=>setData(json));
+  return (
+    <div>App
+      <input type='text' placeholder='search' onChange={(e)=>setQuery(e.target.value)}/>
+    {/* {JSON.stringify(data)}; */}
+    {data.filter((user)=>user.login.toLowerCase().includes(query)).map((item) => 
+      <tr><td>{item.login}</td></tr>
+    )}
+    <h1>Hello World</h1>
+    </div>
+
+  )
+}
+
+export default App
